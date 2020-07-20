ff1291458a5f345c65e3d49b8d2df907a99ee11f
Samhain666 <carrion13@mail.ru>
2020-07-20 17:32:26 +0300

    Create new controller

--- src/main/java/com/myblog/MyBlog/controllers/MainController.java
+++ src/main/java/com/myblog/MyBlog/controllers/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/greeting")
+    @GetMapping("/")
     public String greeting(Model model) {
         model.addAttribute("name", "Главная страница");
         return "greeting";
